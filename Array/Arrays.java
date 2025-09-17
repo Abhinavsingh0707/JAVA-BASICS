f6b0aa1baa27d11f8826cecb8ef30ae5bf27da1f
Abhinav Singh <abhinavsinghinright@gmail.com>
2025-09-18 02:33:03 +0530

    Add files via upload

---
+++ Array/Arrays.java
@@ -0,0 +1,42 @@
+package Arrays;
+public class Arrays {
+    public static void main(String[] args) {
+        // Search and element in an array
+        /*int [] arr = {1,4,2,55,7,-2};
+        for(int i : arr){
+            if(i == 55){
+                System.out.println("found");
+
+            }*/
+        //Find the maximum element 
+        /*int [] arr = new int[5];
+            arr[0] = 2;
+            arr[1] = 12;
+            arr[2] = 4322;
+            arr[3] = -32;
+            arr[4] = 22;
+            int res = Integer.MIN_VALUE;
+            for (int i = 0; i < arr.length; i++) {
+                res = arr[i];
+                if (arr[i] > res) {
+                    res = arr[i];
+                }
+            }
+            System.out.println(res);*/
+        // reverse print array
+        /*int [] arr = {1,3,43,4,345};
+        for(int i = arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+            }*/
+        
+        // Sum of the array
+        /*int [] arr = {-2,4,55,-4,11,-55,-11,2};
+            int sum = 0;
+            for (int i = 0; i < arr.length; i++) {
+                sum += arr[i];
+            }
+            System.out.println(sum);*/
+        
+        }
+    }
+
---
+++ Array/Updation.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class Updation {
+    public static void main(String[] args) {
+        int arr [] = {10,20,30,40,50};
+
+        int indexpos = 2;
+        int element = 100;
+         arr[indexpos] = element;
+
+         for(int no : arr){
+            System.out.println(no+" ");
+         }
+    }
+}
